ec6e16cac858bf03ec22d0bc2f5f72c6ac257522
unknown <toto@Poste-W091.objectif.www>
2023-05-25 09:36:44 +0200

    second commit

--- exo_filtre_auteur.php
+++ exo_filtre_auteur.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exo Filtre auteur</title>
+    <title>Exo Filtre auteurs</title>
     <style>
     html{
         font-family : "arial";
