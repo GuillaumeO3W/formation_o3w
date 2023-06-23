88331b5e318df658e084d7f9e6662b747b780963
Guillaume <gu.charrier@gmail.com>
2023-06-23 15:20:39 +0200

    maj

--- calcul_taxe.php
+++ calcul_taxe.php
@@ -6,6 +6,13 @@
     <title>Calcul Taxe</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <style>
+        form input{
+            text-align : center;
+            font-family : impact;
+        }
+
+    </style>
 </head>
 <body>
 <?php
@@ -29,19 +36,18 @@ if (isset($_POST['ht']) && isset($_POST['tva'])) {
 ?>
 <div class="container d-flex flex-column min-vh-100 justify-content-center align-items-center">
     <form action="" method="POST" class="border rounded p-4 d-flex flex-column mb-4">
+    <h1 class="display-5 mb-4">Calcul de taxe</h1>
             <div class="mb-3">
-                <label for="ht" class="form-label">montant HT</label>
-                <input type="number" name="ht" class="form-control" id="ht">
+                <input type="number" name="ht" class="form-control" id="ht" placeholder="H T">
             </div>
             <div class="mb-3">
-                <label for="tva" class="form-label">tva</label>
-                <input type="number" name="tva" class="form-control" id="tva">
+                <input type="number" name="tva" class="form-control" id="tva" placeholder="T V A">
             </div>
             <button type="submit" class="btn btn-outline-info">Calcul</button>
     </form>
     <div class="text-center">
         <p><?= 'ht = ' .$ht. '€ / tva = '.$tva. '% / taxe = ' .$taxe .' €' ?></p>
-        <h1><?= 'ttc = ' .$ttc .' €'?></h1>
+        <h2><?= 'ttc = ' .$ttc .' €'?></h2>
     </div>
 </div>
 <pre class="d-none"><?php var_dump($_POST) ?></pre>
