b418168eaf7e77fe9902b1fa5896506ed0c8dead
GuillaumeO3W <gu.charrier@gmail.com>
2023-06-24 11:31:01 +0200

    maj

--- convertisseur_numerique.php
+++ convertisseur_numerique.php
@@ -33,7 +33,6 @@ II donnerait 2 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Convertisseur numérique</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
 </head>
 <body>
 <?php
@@ -46,9 +45,6 @@ if(isset($_GET['reset'])){
 }
 
 $input='MMDCCCLXXIX'; //2879
-// $input='MCMLIV'; 
-// $input='LXXIX';
-// $input='MCM';
 
 if(ctype_alpha($input)):
     $split = array_reverse(str_split($input));
