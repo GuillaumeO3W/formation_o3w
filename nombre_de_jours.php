ed85eecc93c6819d17bafdf243d498a79e0bd5e8
Guillaume <gu.charrier@gmail.com>
2023-06-29 12:01:13 +0200

    maj

--- nombre_de_jours.php
+++ nombre_de_jours.php
@@ -7,7 +7,6 @@ Concevoir un programme qui demande deux dates à l’utilisateur et calcule le n
 jours les séparant (on ne tiendra pas compte des années bissextiles)
 /!\ Il est bon de se rappeler que les mois n’ont pas tous le même nombre de jours -->
 
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -18,6 +17,33 @@ jours les séparant (on ne tiendra pas compte des années bissextiles)
 </head>
 <body>
 <?php
+
+if(isset($_GET[reset])):
+    $_SERVER 
+
+isset($_POST['date1']) ? $date1=$_POST['date1'] : $date1=null;
+isset($_POST['date2']) ? $date2=$_POST['date2'] : $date2=null;
 ?>
+<div class="d-flex flex-column min-vh-100 justify-content-center align-items-center gap-4">
+    <form action="" method="POST" class="d-flex flex-column gap-2  border border-warning rounded p-4 shadow-sm">
+        
+        <div class="d-flex justify-content-between gap-2">
+            <label for="date1">Date de départ : </label>
+            <input type="date" id="date1" name="date1">
+        </div>
+
+        <div class="d-flex justify-content-between gap-2">
+            <label for="date2">Date de fin :</label>
+            <input type="date" id="date2" name="date2">
+        </div>
+        
+        <div class="d-flex justify-content-center gap-2 mt-4">
+            <input type="submit" class="btn btn-outline-info">
+            <a href="?reset" class="btn btn-outline-warning" >Reset</a>
+        </div>
+    </form>
+    <?= $date1.' -- '.$date2; ?>
+</div>
+
 </body>
 </html>
