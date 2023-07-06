77a797235aa5e92534f62923902fac255236929b
Guillaume <gu.charrier@gmail.com>
2023-07-06 10:04:13 +0200

    maj

--- tombola/index.php
+++ tombola/index.php
@@ -1,14 +1,29 @@
 <?php
 include ('inc/header.php');
 include ('lib/functions.php');
+if(isset($_GET['reset'])){
+    unset($_SESSION['tombola']);
+    header ('location: index.php');
+    exit;
+}
+if(isset($_GET['newGame'])){
+    header ('location: index.php');
+    exit;
+}
 
 $userMoney = 120;
 $userQuantity = 20;
+
 $quantity = quantity($userQuantity, $userMoney);
 $tickets = tickets($quantity);
 $tirages=tirages();
+$results=(results($tickets , $tirages));
+$gains = gains($results);
 ?>
-
+<div>
+        <a href="?reset">reset</a>
+        <a href="?newGame">Nouvelle partie</a>
+</div>
 <div class="main">
     <div>
         <h3>Tickets</h3>
@@ -24,10 +39,14 @@ $tirages=tirages();
     </div>
     <div>
         <h3>Résultat</h3>
-        <pre><?php print_r(results($tickets , $tirages)) ;?></pre>
+        <pre><?php print_r($results) ;?></pre>
     </div>
-    
+    <div>
+        <h3>Gains</h3>
+        <pre><?php print_r($gains) ;?></pre>
+    </div>
 </div>
+
 
 
 <?php
--- tombola/lib/functions.php
+++ tombola/lib/functions.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 function quantity($quantity, $money){
     $money>=200 ? $ticketAvaible = 100 : $ticketAvaible = $money/2;
     $quantity >= $ticketAvaible ? $quantity = $ticketAvaible : $quantity = $quantity;
@@ -37,5 +39,21 @@ function results($tickets , $tirages){
         }
     }
     return $results;
+}
+
+function gains($results){
+    !isset($_SESSION['tombola']['gains']) ? $_SESSION['tombola']['gains'] = null : '' ;
+    foreach ($results as $ticket => $rank){
+        if($rank == 0){
+            $_SESSION['tombola']['gains'] += 100;
+        }elseif($rank == 1){
+            $_SESSION['tombola']['gains'] += 50;
+        }elseif($rank == 2){
+            $_SESSION['tombola']['gains'] += 20;
+        }else{
+            $_SESSION['tombola']['gains'] += 0;
+        }
+    }
+    return $_SESSION['tombola']['gains'];
 }
 ?>
