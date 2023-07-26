a4608f2deb71ca7c356e833c9bdca760c5130e16
GuillaumeO3W <gu.charrier@gmail.com>
2023-07-26 11:32:53 +0200

    maj

--- sql/02_Creer_requetes/02_Creer_requetes.sql
+++ sql/02_Creer_requetes/02_Creer_requetes.sql
@@ -1,9 +1,17 @@
 -- Exercice SQL 
 -- 1. Les 10 derniers msg de l'utilisateur d'id =  10  
 
-SELECT u.u_id, m.m_contenu, m.m_date
+SELECT *
+FROM message
+WHERE m_auteur_fk=10
+ORDER BY m_date DESC
+LIMIT 10
+
+-- ou
+
+SELECT m.m_id, m.m_contenu, m.m_date , m.m_auteur_fk, m.m_conversation_fk
 FROM message m
-JOIN user u
+LEFT JOIN user u
 ON (u.u_id = m.m_auteur_fk)
 WHERE m_auteur_fk=10
 ORDER BY m.m_date DESC
@@ -11,19 +19,38 @@ LIMIT 10
 
 -- 2. La liste des utilisateurs triés par age  
 
-
+SELECT u_nom, u_prenom, u_date_naissance
+FROM user
+ORDER BY u_date_naissance
 
 -- 3. Les 5 derniers inscrits  
 
-
+SELECT *
+FROM user
+ORDER BY u_date_inscription DESC
+LIMIT 5
 
 -- 4. Les 20 derniers messages avec l'utilisateur(login) associé et son rang 
 
-
+SELECT m.m_contenu, u.u_login, r.r_libelle
+FROM message m
+JOIN user u
+ON m.m_auteur_fk = u.u_id
+JOIN rang r
+ON u.u_rang_fk = r.r_id
+ORDER BY m.m_date DESC
+LIMIT 20
 
 -- 5. Les 5 derniers messages des utilisateurs de rang admin de plus de 18ans  
 
-
+SELECT *
+FROM message m
+JOIN user u
+ON m.m_auteur_fk = u.u_id AND TIMESTAMPDIFF(YEAR, u.u_date_naissance, CURDATE()) >= 18
+JOIN rang r
+ON u.u_rang_fk = r.r_id AND r.r_libelle = 'admin'
+ORDER BY m.m_date DESC
+LIMIT 5
 
 -- 6. Les 10 derniers messages avec login+N° de conversation des user agés de 18 à 30 ans 
 
