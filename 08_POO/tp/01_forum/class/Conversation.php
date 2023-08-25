5e8ef3f47a147b2d70130e339a195cae94f6abf6
Guillaume <gu.charrier@gmail.com>
2023-08-25 16:56:38 +0200

    maj

--- 08_POO/tp/01_forum/class/Conversation.php
+++ 08_POO/tp/01_forum/class/Conversation.php
@@ -2,5 +2,53 @@
 
 class Conversation
 {
+    private $_id;
+    private $_date;
+    private $_termine;
     
+    public function __construct($data)
+    {     
+        $this->hydrate($data);
+    }
+    
+    public function getId()
+    {
+        return $this->_id;
+    }
+    public function setId($id){
+        $this->_id = $id;
+        return $this;
+    }
+    
+    public function getDate()
+    {
+        return $this->_date;
+    }
+    public function setDate($date){
+        $this->_date = $date;
+        return $this;
+    }
+    
+    public function getTermine()
+    {
+        return $this->_termine;
+    }
+    public function setTermine($termine){
+        $this->_termine = $termine;
+        return $this;
+    }
+    
+    
+    public function hydrate($data)
+    {
+        foreach($data as $key => $value)
+        {
+            $methodName = 'set'. ucfirst(substr($key, 2, strlen($key)-2));
+            if(method_exists($this, $methodName))
+            {
+                $this->$methodName($value);
+            }
+        }
+    }
+
 }
--- 08_POO/tp/01_forum/index.php
+++ 08_POO/tp/01_forum/index.php
@@ -2,13 +2,21 @@
 require 'inc/head.php';
 require 'lib/_helpers/tools.php';
 require 'lib/functions.php';
+require 'class/ConversationModel.php';
+require 'class/Conversation.php';
 
 try
 {
 
     $conversationModel = new ConversationModel;
     $conversationModel->readAll(); 
+    foreach($conversations as $conversation):
+        echo $conversation->getId();
+        echo $conversation->getDate();
+        echo $conversation->getTermine();
 
+
+    endforeach;
 
 }
 catch(PDOException $e)
