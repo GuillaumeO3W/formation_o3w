e783821b69675b5ed29d69b141a2c7623f9e6a2f
GuillaumeO3W <gu.charrier@gmail.com>
2023-09-12 16:06:27 +0200

    maj

--- 09_MVC_exoNains/Views/nains/nainView.php
+++ 09_MVC_exoNains/Views/nains/nainView.php
@@ -12,9 +12,31 @@ if(!empty($nain)) :
         <?= $nain->getNom().", ".$nain->getBarbe() ?>
       </div>
       <p>Originaire de <a href="index.php?ctrl=ville&action=villeView&id=<?= $nain->getV_id() ?>"><?= $nain->getVille() ?></a></p>
+
       <?= $nain->getTaverne() == "0" ? "" : "<p>Boit dans <a href=\"index.php?ctrl=taverne&action=taverneView&id=".$nain->getT_id()."\">".$nain->getTaverne()."</a></p>" ?>
-      <p>Travaille de <?= $nain->getDebut()." à ".$nain->getFin() ?> dans le tunnel de <?= "<a href=\"index.php?ctrl=ville&action=villeView&id=".$nain->getDepartId() ."\">".$nain->getDepart()."</a> à <a href=\"index.php?ctrl=ville&action=villeView&id=".$nain->getArriveeId() ."\">".$nain->getArrivee()."</a>" ?></p>
-      <p>Membre du groupe <?= $nain->getGroupe() == "0" ? "Aucun groupe" : "<a href=\"index.php?ctrl=groupe&action=groupeView&id=".$nain->getGroupe()."\">n°".$nain->getGroupe()."</a>" ?></p>     
+      
+      <p>Travaille de <?= $nain->getDebut()." à ".$nain->getFin() ?>
+         dans le tunnel de 
+         <a href="index.php?ctrl=ville&action=villeView&id=<?= $nain->getDepartId()?>"><?= $nain->getDepart()?></a>
+         à 
+         <a href="index.php?ctrl=ville&action=villeView&id=<?=$nain->getArriveeId()?>"><?=$nain->getArrivee()?></a>
+      </p>
+       
+      <?php 
+      if($nain->getGroupe() != "0"):
+      ?>
+        <p>Membre du groupe 
+          <a href="index.php?ctrl=groupe&action=groupeView&id=<?= $nain->getGroupe() ?>">
+            n° <?= $nain->getGroupe()?>
+          </a>
+        </p>  
+      <?php 
+      else : 
+      ?>  
+        <p>Aucun groupe</p>
+      <?php 
+      endif; 
+      ?>  
 
 <?php   else: ?>
           <p>Aucun nain</p>
